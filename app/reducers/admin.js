16a68649f00afa5138f1e58a9c17738a22c6c48b
Hai Dong <haidongdo1994@gmail.com>
2016-06-16 10:20:31 +0700

    Update reducer using merge instead of assign to keep state

--- app/reducers/admin.js
+++ app/reducers/admin.js
@@ -36,54 +36,59 @@ export const admin = (state = initialState, action) => {
   const { type, response } = action;
   switch (type) {
     case AdminActionTypes.ADMIN_GET_USERS_REQUEST:
-      return _.assign({}, state, {
+      return _.merge({}, state, {
         userManagement: {
-          isFetching: true
+          isFetching: true,
+          selectedUser: {},
+          submitResult: '',
+          shopList: []
         }
       });
     case AdminActionTypes.ADMIN_GET_USERS_SUCCESS:
-      return _.assign({}, state, {
+      return _.merge({}, state, {
         userManagement: {
           isFetching: false,
           userList: response.users
         }
       });
     case AdminActionTypes.ADMIN_GET_USER_REQUEST:
-      return _.assign({}, state, {
+      return _.merge({}, state, {
         userManagement: {
-          isFetching: true
+          isFetching: true,
+          selectedUser: {},
+          submitResult: false
         }
       });
     case AdminActionTypes.ADMIN_GET_USER_SUCCESS:
-      return _.assign({}, state, {
+      return _.merge({}, state, {
         userManagement: {
           isFetching: false,
           selectedUser: response
         }
       });
     case AdminActionTypes.ADMIN_GET_USERS_FAILURE:
-      return _.assign({}, state, { users: [] });
+      return _.merge({}, state, { users: [] });
     case AdminActionTypes.ADMIN_UPDATE_USER_INFORMATION_SUCCESS:
-      return _.assign({}, state, {
+      return _.merge({}, state, {
         userManagement: {
           selectedUser: response,
           submitResult: AsyncResultCode.UPDATE_USER_INFORMATION_SUCCESS
         }
       });
     case AdminActionTypes.ADMIN_UPDATE_USER_INFORMATION_FAILURE:
-      return _.assign({}, state, {
+      return _.merge({}, state, {
         userManagement: {
           submitResult: AsyncResultCode.UPDATE_USER_INFORMATION_FAIL
         }
       });
     case AdminActionTypes.ADMIN_UPDATE_USER_ROLE_REQUEST:
-      return _.assign({}, state, {
+      return _.merge({}, state, {
         userManagement: {
           isSubmitting: true
         }
       });
     case AdminActionTypes.ADMIN_UPDATE_USER_ROLE_SUCCESS:
-      return _.assign({}, state, {
+      return _.merge({}, state, {
         userManagement: {
           selectedUser: response,
           isSubmitting: false,
@@ -91,93 +96,95 @@ export const admin = (state = initialState, action) => {
         }
       });
     case AdminActionTypes.ADMIN_UPDATE_USER_ROLE_FAILURE:
-      return _.assign({}, state, {
+      return _.merge({}, state, {
         userManagement: {
           isSubmitting: false,
           submitResult: AsyncResultCode.UPDATE_USER_ROLE_FAIL
         }
       });
     case AdminActionTypes.ADMIN_BAN_USER_REQUEST:
-      return _.assign({}, state, {
+      return _.merge({}, state, {
         userManagement: {
-          isSubmitting: true,
-          selectedUser: state.userManagement.selectedUser
+          isSubmitting: true
         }
       });
     case AdminActionTypes.ADMIN_BAN_USER_SUCCESS:
-      return _.assign({}, state, {
+      return _.merge({}, state, {
         userManagement: {
           selectedUser: response,
           submitResult: AsyncResultCode.BAN_USER_SUCCESS
         }
       });
     case AdminActionTypes.ADMIN_BAN_USER_FAILURE:
-      return _.assign({}, state, {
+      return _.merge({}, state, {
         userManagement: {
           submitResult: AsyncResultCode.BAN_USER_FAIL
         }
       });
     case AdminActionTypes.ADMIN_UNBAN_USER_REQUEST:
-      return _.assign({}, state, {
+      return _.merge({}, state, {
         userManagement: {
-          isSubmitting: true,
-          selectedUser: state.userManagement.selectedUser
+          isSubmitting: true
         }
       });
     case AdminActionTypes.ADMIN_UNBAN_USER_SUCCESS:
-      return _.assign({}, state, {
+      return _.merge({}, state, {
         userManagement: {
           selectedUser: response,
           submitResult: AsyncResultCode.UNBAN_USER_SUCCESS
         }
       });
     case AdminActionTypes.ADMIN_UNBAN_USER_FAILURE:
-      return _.assign({}, state, {
+      return _.merge({}, state, {
         userManagement: {
           submitResult: AsyncResultCode.UNBAN_USER_FAIL
         }
       });
     case AdminActionTypes.ADMIN_GET_SHOPS_REQUEST:
-      return _.assign({}, state, {
+      return _.merge({}, state, {
         shopManagement: {
-          isFetching: true
+          isFetching: true,
+          submitResult: '',
+          shopList: []
         }
       });
     case AdminActionTypes.ADMIN_GET_SHOPS_SUCCESS:
-      return _.assign({}, state, {
+      return _.merge({}, state, {
         shopManagement: {
           isFetching: false,
           shopList: response.shops
         }
       });
     case AdminActionTypes.ADMIN_GET_SHOPS_FAILURE:
-      return _.assign({}, state, {
+      return _.merge({}, state, {
         shopManagement: {
           isFetching: false,
           shopList: []
         }
       });
     case AdminActionTypes.ADMIN_GET_SHOP_REQUEST:
-      return _.assign({}, state, {
+      return _.merge({}, state, {
         shopManagement: {
-          isFetching: true
+          isFetching: true,
+          submitResult: '',
+          selectedShop: {}
         }
       });
     case AdminActionTypes.ADMIN_GET_SHOP_SUCCESS:
-      return _.assign({}, state, {
+      return _.merge({}, state, {
         shopManagement: {
           isFetching: false,
           selectedShop: response
         }
       });
     case AdminActionTypes.ADMIN_UPDATE_SHOP_INFORMATION_REQUEST:
-      return _.assign({}, state, {
+      return _.merge({}, state, {
         shopManagement: {
           isSubmitting: true
         }
       });
     case AdminActionTypes.ADMIN_UPDATE_SHOP_INFORMATION_SUCCESS:
-      return _.assign({}, state, {
+      return _.merge({}, state, {
         shopManagement: {
           isSubmitting: false,
           selectedShop: response,
@@ -185,21 +192,20 @@ export const admin = (state = initialState, action) => {
         }
       });
     case AdminActionTypes.ADMIN_UPDATE_SHOP_INFORMATION_FAILURE:
-      return _.assign({}, state, {
+      return _.merge({}, state, {
         shopManagement: {
           isSubmitting: false,
           submitResult: AsyncResultCode.UPDATE_SHOP_INFORMATION_FAIL
         }
       });
     case AdminActionTypes.ADMIN_UPDATE_SHOP_AVATAR_REQUEST:
-      return _.assign({}, state, {
+      return _.merge({}, state, {
         shopManagement: {
-          isSubmitting: true,
-          selectedShop: state.shopManagement.selectedShop
+          isSubmitting: true
         }
       });
     case AdminActionTypes.ADMIN_UPDATE_SHOP_AVATAR_SUCCESS:
-      return _.assign({}, state, {
+      return _.merge({}, state, {
         shopManagement: {
           isSubmitting: false,
           selectedShop: response,
@@ -207,21 +213,20 @@ export const admin = (state = initialState, action) => {
         }
       });
     case AdminActionTypes.ADMIN_UPDATE_SHOP_AVATAR_FAILURE:
-      return _.assign({}, state, {
+      return _.merge({}, state, {
         shopManagement: {
           isSubmitting: false,
           submitResult: AsyncResultCode.UPDATE_SHOP_AVATAR_FAIL
         }
       });
     case AdminActionTypes.ADMIN_UPDATE_SHOP_COVER_REQUEST:
-      return _.assign({}, state, {
+      return _.merge({}, state, {
         shopManagement: {
-          isSubmitting: true,
-          selectedShop: state.shopManagement.selectedShop
+          isSubmitting: true
         }
       });
     case AdminActionTypes.ADMIN_UPDATE_SHOP_COVER_SUCCESS:
-      return _.assign({}, state, {
+      return _.merge({}, state, {
         shopManagement: {
           isSubmitting: false,
           selectedShop: response,
@@ -229,128 +234,126 @@ export const admin = (state = initialState, action) => {
         }
       });
     case AdminActionTypes.ADMIN_UPDATE_SHOP_COVER_FAILURE:
-      return _.assign({}, state, {
+      return _.merge({}, state, {
         shopManagement: {
           isSubmitting: false,
           submitResult: AsyncResultCode.UPDATE_SHOP_AVATAR_FAIL
         }
       });
     case AdminActionTypes.ADMIN_BAN_SHOP_REQUEST:
-      return _.assign({}, state, {
+      return _.merge({}, state, {
         shopManagement: {
-          isSubmitting: true,
-          selectedShop: state.shopManagement.selectedShop
+          isSubmitting: true
         }
       });
     case AdminActionTypes.ADMIN_BAN_SHOP_SUCCESS:
-      return _.assign({}, state, {
+      return _.merge({}, state, {
         shopManagement: {
           selectedShop: response,
           submitResult: AsyncResultCode.BAN_SHOP_SUCCESS
         }
       });
     case AdminActionTypes.ADMIN_BAN_SHOP_FAILURE:
-      return _.assign({}, state, {
+      return _.merge({}, state, {
         shopManagement: {
-          submitResult: AsyncResultCode.BAN_SHOP_FAIL,
-          selectedShop: state.shopManagement.selectedShop
+          submitResult: AsyncResultCode.BAN_SHOP_FAIL
         }
       });
     case AdminActionTypes.ADMIN_UNBAN_SHOP_REQUEST:
-      return _.assign({}, state, {
+      return _.merge({}, state, {
         shopManagement: {
-          isSubmitting: true,
-          selectedShop: state.shopManagement.selectedShop
+          isSubmitting: true
         }
       });
     case AdminActionTypes.ADMIN_UNBAN_SHOP_SUCCESS:
-      return _.assign({}, state, {
+      return _.merge({}, state, {
         shopManagement: {
           selectedShop: response,
           submitResult: AsyncResultCode.UNBAN_SHOP_SUCCESS
         }
       });
     case AdminActionTypes.ADMIN_UNBAN_SHOP_FAILURE:
-      return _.assign({}, state, {
+      return _.merge({}, state, {
         shopManagement: {
-          submitResult: AsyncResultCode.UNBAN_SHOP_FAIL,
-          selectedShop: state.shopManagement.selectedShop
+          submitResult: AsyncResultCode.UNBAN_SHOP_FAIL
         }
       });
     case AdminActionTypes.ADMIN_GET_REQUESTS_REQUEST:
-      return _.assign({}, state, {
+      return _.merge({}, state, {
         requestManagement: {
-          isFetching: true
+          isFetching: true,
+          submitResult: '',
+          requestList: []
         }
       });
     case  AdminActionTypes.ADMIN_GET_REQUESTS_SUCCESS:
-      return _.assign({}, state, {
+      return _.merge({}, state, {
         requestManagement: {
           isFetching: false,
           requestList: response.shopOpeningRequests
         }
       });
     case AdminActionTypes.ADMIN_GET_REQUESTS_FAILURE:
-      return _.assign({}, state, {
+      return _.merge({}, state, {
         requestManagement: {
           isFetching: false
         }
       });
     case AdminActionTypes.ADMIN_ACCEPT_REQUEST_REQUEST:
-      return _.assign({}, state, {
+      return _.merge({}, state, {
         requestManagement: {
           isSubmitting: true
         }
       });
     case AdminActionTypes.ADMIN_ACCEPT_REQUEST_SUCCESS:
-      return _.assign({}, state, {
+      return _.merge({}, state, {
         requestManagement: {
           isSubmitting: false,
           submitResult: 'OK'
         }
       });
     case AdminActionTypes.ADMIN_ACCEPT_REQUEST_FAILURE:
-      return _.assign({}, state, {
+      return _.merge({}, state, {
         requestManagement: {
           isSubmitting: false,
           submitResult: response.message_code
         }
       });
     case AdminActionTypes.ADMIN_REJECT_REQUEST_REQUEST:
-      return _.assign({}, state, {
+      return _.merge({}, state, {
         requestManagement: {
           isSubmitting: true
         }
       });
     case AdminActionTypes.ADMIN_REJECT_REQUEST_SUCCESS:
-      return _.assign({}, state, {
+      return _.merge({}, state, {
         requestManagement: {
           isSubmitting: false,
           submitResult: 'OK'
         }
       });
     case AdminActionTypes.ADMIN_REJECT_REQUEST_FAILURE:
-      return _.assign({}, state, {
+      return _.merge({}, state, {
         requestManagement: {
           isSubmitting: false,
           submitResult: response.message_code
         }
       });
     case ActionTypes.ADMIN_CHANGE_PASSWORD_REQUEST:
-      return _.assign({}, state, {
+      return _.merge({}, state, {
         changePasswordFormStatus: {
           isSubmitting: true
         }
       });
     case ActionTypes.ADMIN_CHANGE_PASSWORD_SUCCESS:
-      return _.assign({}, state, {
+      return _.merge({}, state, {
         changePasswordFormStatus: {
           isSubmitting: false,
           response: ''
         }
       });
     case ActionTypes.ADMIN_CHANGE_PASSWORD_FAILURE:
-      return _.assign({}, state, {
+      return _.merge({}, state, {
         changePasswordFormStatus: {
           isSubmitting: false,
           response: 'Error happened'

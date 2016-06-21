8087df1ba4777770914101f9383145fb74912d3c
Hieu Tran <658655@gmail.com>
2016-06-21 17:55:48 +0700

    Markup seller dashboard

--- app/components/home/BlockSellerDashboardSideBar/BlockSellerDashboardSideBar.jsx
+++ app/components/home/BlockSellerDashboardSideBar/BlockSellerDashboardSideBar.jsx
@@ -1,6 +1,8 @@
 import React, { Component, PropTypes } from 'react';
 import { ListGroup, ListGroupItem } from 'react-bootstrap';
 import { LinkContainer } from 'react-router-bootstrap';
+import './BlockSellerDashboardSideBar.scss';
+
 
 export default class BlockSellerDashboardSideBar extends Component {
   constructor(props) {
@@ -37,20 +39,49 @@ export default class BlockSellerDashboardSideBar extends Component {
   render() {
     const { shopID } = this.props;
     return (
-      <ListGroup>
+      <ListGroup className="block-seller-dashboard-sidebar">
         <ListGroupItem href="#">
-          <div className="checkbox checkbox-slider--b checkbox-slider-">
-            <label>
-              <input type="checkbox"
-                     checked={this.state.opening}
-                     onChange={this.openingStatusChanged} /><span>Shop opening status</span>
-            </label>
+          <div className="clearfix action">
+            <div className="icon icon-orange">
+              <i className="fa fa-check-circle-o"/>
+            </div>
+            <div className="title">
+              Opening Status
+            </div>
+            <div className="checkbox checkbox-slider--b control">
+              <label>
+                <input type="checkbox"
+                       checked={this.state.opening}
+                       onChange={this.openingStatusChanged} /><span/>
+              </label>
+            </div>
           </div>
+
         </ListGroupItem>
         <LinkContainer to={`/shops/${shopID}/dashboard/info`}>
-          <ListGroupItem>Update shop description</ListGroupItem>
+          <ListGroupItem>
+            <div className="clearfix action">
+              <div className="icon icon-gray">
+                <i className="fa fa-cog"/>
+              </div>
+              <div className="title">
+                Update shop
+              </div>
+            </div>
+          </ListGroupItem>
         </LinkContainer>
-        <ListGroupItem href="#">Manage selling items</ListGroupItem>
+        <LinkContainer to={`/shops/${shopID}/dashboard/info`}>
+          <ListGroupItem>
+            <div className="clearfix action">
+              <div className="icon icon-green">
+                <i className="fa fa-plus-square-o"/>
+              </div>
+              <div className="title">
+                Add selling item
+              </div>
+            </div>
+          </ListGroupItem>
+        </LinkContainer>
       </ListGroup>
     )
   }

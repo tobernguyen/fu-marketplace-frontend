c1bde9bd3f947ceb976dc7732ff267709f403049
Hai Dong <haidongdo1994@gmail.com>
2016-07-03 20:20:22 +0700

    [r] Refactor url to match with current router setting

--- app/components/home/BlockOrderListHeader/BlockOrderListHeader.jsx
+++ app/components/home/BlockOrderListHeader/BlockOrderListHeader.jsx
@@ -8,28 +8,28 @@ const BlockOrderListHeader = ({ shopID }) => {
     <div className="header clearfix">
         <ul className="nav nav-pills">
           <li>
-            <Link to={{ pathname: `shops/${shopID}/dashboard/orders` , query: { status: 'all'} }} activeClassName="active"><FormattedMessage {...messages.orderList.tableBody.orderStatus.all}/></Link>
+            <Link to={{ pathname: `/shops/${shopID}/dashboard/orders` , query: { status: 'all'} }} activeClassName="active"><FormattedMessage {...messages.orderList.tableBody.orderStatus.all}/></Link>
           </li>
           <li>
-            <Link to={{ pathname: `shops/${shopID}/dashboard/orders` , query: { status: 'new'} }} activeClassName="active"><FormattedMessage {...messages.orderList.tableBody.orderStatus.new}/></Link>
+            <Link to={{ pathname: `/shops/${shopID}/dashboard/orders` , query: { status: 'new'} }} activeClassName="active"><FormattedMessage {...messages.orderList.tableBody.orderStatus.new}/></Link>
           </li>
           <li>
-            <Link to={{ pathname: `shops/${shopID}/dashboard/orders` , query: { status: 'accepted'} }} activeClassName="active"><FormattedMessage {...messages.orderList.tableBody.orderStatus.accepted}/></Link>
+            <Link to={{ pathname: `/shops/${shopID}/dashboard/orders` , query: { status: 'accepted'} }} activeClassName="active"><FormattedMessage {...messages.orderList.tableBody.orderStatus.accepted}/></Link>
           </li>
           <li>
-            <Link to={{ pathname: `shops/${shopID}/dashboard/orders` , query: { status: 'shipping'} }} activeClassName="active"><FormattedMessage {...messages.orderList.tableBody.orderStatus.shipping}/></Link>
+            <Link to={{ pathname: `/shops/${shopID}/dashboard/orders` , query: { status: 'shipping'} }} activeClassName="active"><FormattedMessage {...messages.orderList.tableBody.orderStatus.shipping}/></Link>
           </li>
           <li>
-            <Link to={{ pathname: `shops/${shopID}/dashboard/orders` , query: { status: 'completed'} }} activeClassName="active"><FormattedMessage {...messages.orderList.tableBody.orderStatus.completed}/></Link>
+            <Link to={{ pathname: `/shops/${shopID}/dashboard/orders` , query: { status: 'completed'} }} activeClassName="active"><FormattedMessage {...messages.orderList.tableBody.orderStatus.completed}/></Link>
           </li>
           <li>
-            <Link to={{ pathname: `shops/${shopID}/dashboard/orders` , query: { status: 'rejected'} }} activeClassName="active"><FormattedMessage {...messages.orderList.tableBody.orderStatus.rejected}/></Link>
+            <Link to={{ pathname: `/shops/${shopID}/dashboard/orders` , query: { status: 'rejected'} }} activeClassName="active"><FormattedMessage {...messages.orderList.tableBody.orderStatus.rejected}/></Link>
           </li>
           <li>
-            <Link to={{ pathname: `shops/${shopID}/dashboard/orders` , query: { status: 'canceled'} }} activeClassName="active"><FormattedMessage {...messages.orderList.tableBody.orderStatus.canceled}/></Link>
+            <Link to={{ pathname: `/shops/${shopID}/dashboard/orders` , query: { status: 'canceled'} }} activeClassName="active"><FormattedMessage {...messages.orderList.tableBody.orderStatus.canceled}/></Link>
           </li>
           <li>
-            <Link to={{ pathname: `shops/${shopID}/dashboard/orders` , query: { status: 'aborted'} }} activeClassName="active"><FormattedMessage {...messages.orderList.tableBody.orderStatus.aborted}/></Link>
+            <Link to={{ pathname: `/shops/${shopID}/dashboard/orders` , query: { status: 'aborted'} }} activeClassName="active"><FormattedMessage {...messages.orderList.tableBody.orderStatus.aborted}/></Link>
           </li>
         </ul>
       </div>

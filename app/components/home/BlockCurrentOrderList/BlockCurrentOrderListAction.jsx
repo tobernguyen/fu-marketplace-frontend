9bd576626f951e233907578e2514c2bc9f65546d
Hai Dong <haidongdo1994@gmail.com>
2016-07-20 22:09:15 +0700

    [f] Show seller message in order card at current order list

--- app/components/home/BlockCurrentOrderList/BlockCurrentOrderListAction.jsx
+++ app/components/home/BlockCurrentOrderList/BlockCurrentOrderListAction.jsx
@@ -36,7 +36,7 @@ const BlockCurrentOrderListAction = ({ order, acceptOrder, rejectOrder, startShi
       );
       break;
     case OrderStatus.REJECTED:
-      output = <OrderStatusMessage message={formatMessage(messages.modalViewOrder.orderStatus.rejected)}/>
+      output = <OrderStatusMessage message={formatMessage(messages.modalViewOrder.orderStatus.rejected)} sellerMessage={order.sellerMessage}/>
       break;
     case OrderStatus.SHIPPING:
       output = (
@@ -64,7 +64,7 @@ const BlockCurrentOrderListAction = ({ order, acceptOrder, rejectOrder, startShi
       output = <OrderStatusMessage message={formatMessage(messages.modalViewOrder.orderStatus.canceled)}/>
     break;
     case OrderStatus.ABORTED:
-      output = <OrderStatusMessage message={formatMessage(messages.modalViewOrder.orderStatus.aborted)}/>
+      output = <OrderStatusMessage message={formatMessage(messages.modalViewOrder.orderStatus.aborted)} sellerMessage={order.sellerMessage}/>
       break;
     default:
       output = <OrderStatusMessage message={formatMessage(messages.modalViewOrder.orderStatus.noStatus)}/>

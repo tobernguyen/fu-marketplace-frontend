874d7dcab9a16fbc9252ae11f8a0b0f32503fc76
Hieu Tran <658655@gmail.com>
2016-08-02 22:42:49 +0700

    Remove fancy background of login page
    [#127537629]

--- app/components/admin/LoginForm/LoginForm.jsx
+++ app/components/admin/LoginForm/LoginForm.jsx
@@ -3,12 +3,10 @@ import './LoginForm.scss';
 
 class LoginForm extends Component {
   render() {
-    const { fields: {email, password}, handleSubmit, submitting } = this.props;
+    const { fields: {email, password}, handleSubmit, invalid } = this.props;
 
     return (
       <div>
-        <div className="stars"></div>
-        <div className="twinkling"></div>
         <div className="login-page">
           <div className="container">
             <div className="login-box col-md-4 col-md-offset-4 col-sm-6 col-sm-offset-3">
@@ -19,7 +17,7 @@ class LoginForm extends Component {
                 <div className="panel-body">
                   <form className="form-horizontal" onSubmit={handleSubmit(this.props.signInAdmin.bind(this))}>
                     <div className={`input-group ${email.touched && email.invalid ? 'has-error' : ''}`}>
-                      <span className="input-group-addon"><i className="glyphicon glyphicon-user"></i></span>
+                      <span className="input-group-addon"><i className="glyphicon glyphicon-user"/></span>
                       <input type="text" className="form-control" placeholder="Email" {...email} />
                     </div>
                     <div className="has-error message">
@@ -27,9 +25,8 @@ class LoginForm extends Component {
                         {email.touched ? email.error : ''}
                       </div>
                     </div>
-
                     <div className={`input-group ${password.touched && password.invalid ? 'has-error' : ''}`}>
-                      <span className="input-group-addon"><i className="glyphicon glyphicon-lock"></i></span>
+                      <span className="input-group-addon"><i className="glyphicon glyphicon-lock"/></span>
                       <input type="password" className="form-control" placeholder="Password" {...password}/>
                     </div>
                     <div className="has-error message">
@@ -37,11 +34,10 @@ class LoginForm extends Component {
                         {password.touched ? password.error : ''}
                       </div>
                     </div>
-
                     <div className="form-group">
                       <div className="col-sm-12">
-                        <button type="submit" className="btn btn-primary btn-login" disabled={submitting}>
-                          <i className="glyphicon glyphicon-log-in"></i> Log in
+                        <button type="submit" className="btn btn-primary btn-login" disabled={invalid}>
+                          <i className="glyphicon glyphicon-log-in"/> Sign in
                         </button>
                       </div>
                     </div>

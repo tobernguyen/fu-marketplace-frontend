3dc082d3d2711169fa0d0f31b5d1306d5eb74b0b
Hieu Tran <658655@gmail.com>
2016-08-30 10:59:02 +0700

    Add favicon

--- webpack.config.js
+++ webpack.config.js
@@ -163,7 +163,8 @@ if(TARGET === 'staging') {
         { from: path.join(__dirname, 'build_templates', 'manifest.json.staging'), to: 'manifest.json' },
         { from: path.join(__dirname, 'build_templates', 'OneSignalSDKUpdaterWorker.js') },
         { from: path.join(__dirname, 'build_templates', 'OneSignalSDKWorker.js') },
-        { from: path.join(__dirname, 'build_templates', 'logo.png') }
+        { from: path.join(__dirname, 'build_templates', 'logo.png') },
+        { from: path.join(__dirname, 'build_templates/favicon') }
       ])
     ],
     resolve: {
